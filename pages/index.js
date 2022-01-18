bcea5067b2115f7428d6de6c55bdc9d443e14396
James Arnold <j1mes@hotmail.co.uk>
2022-01-18 16:33:22 +0000

    first commit

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import Sidebar from "../components/Sidebar";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Spotify 2.0</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <Sidebar />
+        {/* Centre */}
+      </main>
+      <div>{/* Player */}</div>
+    </div>
+  );
+}
